c3c6f84b5fafc35806fb012daae03b8dd0480c07
Manuel E Carpio <mectwork@gmail.com>
2018-09-01 13:32:14 -0400

    Header created

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
-import React, { Component } from 'react';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
+import Header from "./components/layout/Header";
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <h1>Contact Manager</h1>
+        <Header />        
       </div>
     );
   }
