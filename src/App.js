44234ce97b1bbbbe62b645f00b9e06b4563f2593
Manuel E Carpio <mectwork@gmail.com>
2018-09-08 09:15:48 -0400

    Moving Contacts components into a folder

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
 import React, { Component } from "react";
 import "./App.css";
 import Header from "./components/layout/Header";
-import Contacts from "./components/Contacts";
+import Contacts from "./components/contacts/Contacts";
 import { Provider } from "./context";
-import AddContact from "./components/AddContact";
+import AddContact from "./components/contacts/AddContact";
 
 class App extends Component {
   render() {
