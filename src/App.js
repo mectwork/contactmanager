34cd44d40a6e400d1a927bb56cc9e918747e4ecb
Manuel Emilio Carpio <mectwork@gmail.com>
2018-09-12 11:36:16 -0400

    Added 404 page.

--- src/App.js
+++ src/App.js
@@ -1,33 +1,38 @@
-import React, { Component } from "react";
-import { BrowserRouter, Route, Switch } from "react-router-dom";
+import React, { Component } from 'react'
+import { BrowserRouter, Route, Switch } from 'react-router-dom'
 
-import "./App.css";
-import Header from "./components/layout/Header";
-import Contacts from "./components/contacts/Contacts";
-import { Provider } from "./context";
-import AddContact from "./components/contacts/AddContact";
-import EditContact from "./components/contacts/EditContact";
+import './App.css'
+import Header from './components/layout/Header'
+import Contacts from './components/contacts/Contacts'
+import { Provider } from './context'
+import AddContact from './components/contacts/AddContact'
+import EditContact from './components/contacts/EditContact'
+import NotFound from './pages/NotFound'
 
 class App extends Component {
-  render() {
+  render () {
     return (
       <Provider>
         <BrowserRouter>
-          <div className="App">
+          <div className='App'>
             <Header />
-            <div className="container">
+            <div className='container'>
               <Switch>
-                <Route exact path="/" component={Contacts} />
-                <Route exact path="/contacts/add" component={AddContact} />
-                <Route exact path="/contacts/edit/:id" component={EditContact} />
+                <Route exact path='/' component={Contacts} />
+                <Route exact path='/contacts/add' component={AddContact} />
+                <Route
+                  exact
+                  path='/contacts/edit/:id'
+                  component={EditContact}
+                />
+                <Route component={NotFound} />
               </Switch>
             </div>
           </div>
         </BrowserRouter>
       </Provider>
-    );
+    )
   }
 }
 
-export default App;
- 
+export default App
---
+++ src/pages/NotFound.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default () => {
+  return (
+    <React.Fragment>
+      <h1>404 Page.</h1>
+      <p className="lead">This page was not found.</p>
+    </React.Fragment>
+  )
+}
